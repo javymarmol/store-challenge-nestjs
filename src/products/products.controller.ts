5a341253f45f39be7a46bc83ef6547f664251cd7
Heyner Javier Marmol <contacto@javymarmol.com>
2024-02-04 23:51:06 -0500

    - added business logic for CRUD relationship products-stores and endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Param, Delete, HttpCode, Put, HttpStatus, 
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { UpdateProductStoresDto } from './dto/update-product-stores.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -31,5 +32,42 @@ export class ProductsController {
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
+  }
+
+  @Get(':productId/stores')
+  findStoresFromProduct(@Param('productId', ParseIntPipe) productId: number) {
+    return this.productsService.findStoresFromProduct(productId);
+  }
+
+  @Put(':productId/stores')
+  updateStoresFromProduct(
+    @Param('productId', ParseIntPipe) productId: number,
+    @Body() updateProductStoresDto: UpdateProductStoresDto,
+  ) {
+    return this.productsService.updateStoresFromProduct(productId, updateProductStoresDto);
+  }
+
+  @Get(':productId/stores/:storeId')
+  findStoreFromProduct(
+    @Param('productId', ParseIntPipe) productId: number,
+    @Param('storeId', ParseIntPipe) storeId: number,
+  ) {
+    return this.productsService.findStoreFromProduct(productId, storeId);
+  }
+
+  @Post(':productId/stores/:storeId')
+  addStoreToProduct(
+    @Param('productId', ParseIntPipe) productId: number,
+    @Param('storeId', ParseIntPipe) storeId: number,
+  ) {
+    return this.productsService.addStoreToProduct(productId, storeId);
+  }
+
+  @Delete(':productId/stores/:storeId')
+  deleteStoresFromProduct(
+    @Param('productId', ParseIntPipe) productId: number,
+    @Param('storeId', ParseIntPipe) storeId: number,
+  ) {
+    return this.productsService.deleteStoresFromProduct(productId, storeId);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,19 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
+import { Store } from '../stores/entities/store.entity';
+import { UpdateProductStoresDto } from './dto/update-product-stores.dto';
 
 @Injectable()
 export class ProductsService {
-  constructor(@InjectRepository(Product) private productRepository: Repository<Product>) {}
+  constructor(
+    @InjectRepository(Product) private productRepository: Repository<Product>,
+    @InjectRepository(Store) private storeRepository: Repository<Store>,
+  ) {}
+
   create(createProductDto: CreateProductDto) {
     const product = this.productRepository.create(createProductDto);
     return this.productRepository.save(product);
@@ -40,5 +46,101 @@ export class ProductsService {
       throw new NotFoundException(`Product ${id} not found`);
     }
     return this.productRepository.delete(product);
+  }
+
+  async findStoreFromProduct(productId: number, storeId: number) {
+    const product = await this.productRepository.findOne({
+      relations: {
+        stores: true,
+      },
+      where: { id: productId },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product ${productId} not found`);
+    }
+
+    const store = product.stores.find((s) => s.id === storeId);
+
+    if (!store) {
+      throw new BadRequestException(`store ${storeId} is not added to product ${productId}`);
+    }
+
+    return store;
+  }
+
+  async addStoreToProduct(productId: number, storeId: number) {
+    const product = await this.productRepository.findOne({
+      relations: {
+        stores: true,
+      },
+      where: { id: productId },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product ${productId} not found`);
+    }
+
+    const alreadyAdded = product.stores.find((s) => s.id === storeId);
+
+    if (alreadyAdded) {
+      throw new BadRequestException(`store ${storeId} is already added to product ${productId}`);
+    }
+
+    const store = await this.storeRepository.findOneBy({ id: storeId });
+
+    if (!store) {
+      throw new NotFoundException(`Store ${storeId} not found`);
+    }
+
+    product.stores.push(store);
+    return this.productRepository.save(product);
+  }
+
+  async findStoresFromProduct(productId: number) {
+    const product = await this.productRepository.findOne({
+      relations: {
+        stores: true,
+      },
+      where: { id: productId },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product ${productId} not found`);
+    }
+
+    return product.stores;
+  }
+
+  async updateStoresFromProduct(productId: number, payload: UpdateProductStoresDto) {
+    const product = await this.productRepository.findOne({
+      relations: {
+        stores: true,
+      },
+      where: { id: productId },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product ${productId} not found`);
+    }
+
+    product.stores = await this.storeRepository.findBy({ id: In(payload.stores) });
+    return this.productRepository.save(product);
+  }
+
+  async deleteStoresFromProduct(productId: number, storeId: number) {
+    const product = await this.productRepository.findOne({
+      relations: {
+        stores: true,
+      },
+      where: { id: productId },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product ${productId} not found`);
+    }
+
+    product.stores = product.stores.filter((s) => s.id !== storeId);
+    return this.productRepository.save(product);
   }
 }
